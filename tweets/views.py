686d0b9c114e8ae4c2a9eaf9bd7df9089c98a5ea
DaegyuJeong <k5946k@gmail.com>
2024-10-03 05:30:50 +0900

    add urls and templates for all tweets and tweet detail

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
+
 
 # Create your views here.
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "tweets": tweets,
+            "title": "Hello! I am Tweet, My new name is X!",
+        },
+    )
+
+
+def see_one_tweet(request, tweet_pk):
+    try:
+        tweet = Tweet.objects.get(pk=tweet_pk)
+        return render(
+            request,
+            "tweet_detail.html",
+            {
+                "tweet": tweet,
+            },
+        )
+    except Tweet.DoesNotExist:
+        return render(
+            request,
+            "tweet_detail.html",
+            {
+                "not_found": True,
+            },
+        )
