cb1a21c6da5b7a009da52eac05dc34c9a0edf330
DaegyuJeong <k5946k@gmail.com>
2024-10-07 01:32:39 +0900

    ModelSerializer refactoring

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,19 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .serializers import TweetSerializer
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from .models import Tweet
 
 
-@api_view(["GET"])
-def all_tweets(request):
-    tweets = Tweet.objects.all()
-    serializer = TweetSerializer(tweets, many=True)
-    return Response(serializer.data)
+class Tweets(APIView):
+    def get(self, request):
+        tweets = Tweet.objects.all()
+        serializer = TweetSerializer(tweets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TweetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
